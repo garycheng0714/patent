71ba269eaa468548c63908d1a0a2a1bfe9e208d6
GaryCheng <d9369604@gmail.com>
2020-03-07 00:59:10 +0800

    Implement PatentUs class

--- patent.py
+++ patent.py
@@ -4,10 +4,12 @@ from argparse import ArgumentParser
 import pandas as pd
 import re
 import os
+import socket
 
 parser = ArgumentParser()
 parser.add_argument("-n", "--number", help="The number of patent")
 parser.add_argument("-f", "--file", help="Get all images of the patent in the excel file")
+parser.add_argument("-u", "--US", help="The number of U.S. patent")
 args = parser.parse_args()
 
 
@@ -17,6 +19,8 @@ def main():
 
     for number in patent_numbers:
         PatentTw(number).download_all_image()
+
+    # PatentUs('D876,926').download_pdf()
 
 
 def get_patent_numbers_from_excel():
@@ -58,6 +62,34 @@ class PatentTw:
             print(f'Download {file_name}')
 
             urlretrieve(download_url, folder_path + '/' + str(file_name))
+
+
+class PatentUs:
+    base_url = 'https://www.pat2pdf.org'
+
+    def __init__(self, number):
+        self.number = number
+        self.pdf_url = ''
+        self.pdf_name = ''
+        self.__get_pdf_info()
+
+    def __get_pdf_info(self):
+        query = f'/pat2pdf/foo.pl?number={self.number}'
+
+        with urlopen(self.base_url + query, timeout=1) as response:
+            html = response.read()
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        tag = soup.find_all('a', {'href': re.compile(r'/patents/.*')})[0]
+
+        self.pdf_url = tag['href']
+        self.pdf_name = f'{tag.text}.pdf'
+
+    def download_pdf(self):
+        socket.setdefaulttimeout(5)
+        urlretrieve(self.base_url + self.pdf_url, self.pdf_name)
+        print(f'Download: {self.pdf_name}')
 
 
 if __name__ == "__main__":
